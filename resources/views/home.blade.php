6a4c8459aa60bc566474da7ddcfa3bc5ca26dfbb
erfanfh <erfanfarokhi84@gmail.com>
2024-08-30 13:55:38 +0330

    login register home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.master')
 @section('content')
-    <div>Home</div>
+    <img class="header-img" alt="" src="https://patterns.tkdemos.co/wp-content/uploads/2022/06/hed2.jpg">
+    <div class="main-header w-100 d-flex flex-column justify-content-center align-items-center gap-5 text-light">
+        <h1>Let's Play!</h1>
+        <p class="text-center w-75">In this game, you may guess the name of the movie or TV show by it's cover, scene, cast,
+            etc. You can make records, break them, go the leaderboards, play with your friend, challenge them and have fun.<br><span class="fw-bold fs-large">Join and play NOW!</span></p>
+        <a class="btn btn-primary" href="{{ route('register') }}">Sign Up</a>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+})->name('home');
+
+//Auth
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('loginPost', [AuthController::class, 'loginPost'])->name('login.post');
+    Route::get('register', [AuthController::class, 'register'])->name('register');
+    Route::post('registerPost', [AuthController::class, 'registerPost'])->name('register.post');
 });
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
